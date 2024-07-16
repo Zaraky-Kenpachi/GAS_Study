1c7b0ea85b5a6910ade9751a1471375d265ccb10
AlexBH <kenpachi.zaken@gmail.com>
2024-07-16 09:49:23 -0400

    Added Mainhand and Offhand socket. Made the player blueprint and first enemy blueprint with their skeletal mesh and socket.

--- Source/Aura/Private/Character/AuraCharacterBase.cpp
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -5,6 +5,12 @@ AAuraCharacterBase::AAuraCharacterBase()
 {
 	PrimaryActorTick.bCanEverTick = false;
 
+	MainHand = CreateDefaultSubobject<USkeletalMeshComponent>("MainHand");
+	MainHand->SetupAttachment(GetMesh(), FName("MainHandSocket"));
+	MainHand->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	OffHand = CreateDefaultSubobject<USkeletalMeshComponent>("OffHand");
+	OffHand->SetupAttachment(GetMesh(), FName("OffHandSocket"));
+	OffHand->SetCollisionEnabled(ECollisionEnabled::NoCollision);
 }
 
 // Called when the game starts or when spawned
